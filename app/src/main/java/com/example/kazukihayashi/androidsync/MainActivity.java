36f609730054acde99c8f69f43fb2ea793510993
Kazuki <guruguru9606@gmail.com>
2018-09-14 12:21:12 +0900

    Use AlarmManager

--- app/src/main/java/com/example/kazukihayashi/androidsync/MainActivity.java
+++ app/src/main/java/com/example/kazukihayashi/androidsync/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.kazukihayashi.androidsync;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int START_HOUR = 10;
+    private static final int START_MINUTE = 54;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlarmManager alarmManager;
+        PendingIntent pendingIntent;
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, MovieActivity.class);
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, START_HOUR);
+        calendar.set(Calendar.MINUTE, START_MINUTE);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 }
