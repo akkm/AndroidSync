7fd262e4f8f3a919a04a3926090f8b16321d631d
Kazuki <guruguru9606@gmail.com>
2018-09-14 18:00:05 +0900

    Support for TrueTimeRx

--- app/src/main/java/com/example/kazukihayashi/androidsync/MainActivity.java
+++ app/src/main/java/com/example/kazukihayashi/androidsync/MainActivity.java
@@ -7,29 +7,53 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.instacart.library.truetime.TrueTime;
+import com.instacart.library.truetime.TrueTimeRx;
 
 import java.io.IOException;
 import java.util.Calendar;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
-    private static final int START_HOUR = 14;
-    private static final int START_MINUTE = 23;
+    private static final int START_HOUR = 17;
+    private static final int START_MINUTE = 48;
+    private CompositeDisposable mDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Calendar calendar = Calendar.getInstance();
+        final Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
         calendar.set(Calendar.HOUR_OF_DAY, START_HOUR);
         calendar.set(Calendar.MINUTE, START_MINUTE);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
 
-        new TrueTimeAsyncTack().execute(calendar);
+        mDisposable = new CompositeDisposable();
+        mDisposable.add(
+                TrueTimeRx.build()
+                        .initializeRx("time.google.com")
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(date -> new Handler().postDelayed(() -> {
+                            Intent intent = new Intent(getApplicationContext(), MovieActivity.class);
+                            getApplicationContext().startActivity(intent);
+                        }, calendar.getTimeInMillis() - date.getTime())
+                        )
+        );
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDisposable.dispose();
+    }
+
+    @Deprecated
     private class TrueTimeAsyncTack extends AsyncTask<Calendar, Long, Long> {
 
         @Override
@@ -48,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     Intent intent = new Intent(getApplicationContext(), MovieActivity.class);
-                    startActivity(intent);
+                    getApplicationContext().startActivity(intent);
                 }
             }, delay);
         }
